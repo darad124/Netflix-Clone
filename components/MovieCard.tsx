93d33a5ca676ef96f9b72d79d18b363168c4c3c6
darad124 <daryjoe765@gmail.com>
2023-04-26 19:13:22 +0100

    26/04/23

---
+++ components/MovieCard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BsFillPlayFill } from "react-icons/bs";
+
+interface MovieCardsProps {
+  data: Record<string, any>;
+}
+const MovieCard: React.FC<MovieCardsProps> = ({ data }) => {
+  return (
+    <div className="relative group bg-zinc-9pp col-span h-[12vw]">
+      <img
+        className="object-cover w-full transition delay-300 rounded-md shadow-xl cursor-pointer duration group-hover:opacity-90 sm:group-hover:opacity-0 h-[12vw]"
+        src={data.thumbnailUrl}
+        alt="Thumbnail"
+      />
+      <div className="absolute top-0 z-10 invisible w-full transition duration-200 scale-0 opacity-0 sm:visible delay-300 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:opacity-100 group-hover:translate-x-[2vw]">
+        <img
+          className="object-cover w-full transition shadow-xl cursor-pointer rounded-t-md duration h-[12vw]"
+          src={data.thumbnailUrl}
+          alt="Thumbnail"
+        />
+        <div className="absolute w-full p-2 transition rounded-b shadow-md z-index bg-zinc-700 lg:p-4-md">
+          {" "}
+          <div className="flex flex-row items-center gap-3">
+            <div
+              className="flex items-center justify-center w-6 h-6 bg-white rounded-full cursor-pointer lg:w-10 lg:h-10 hover:bg-neutral-300"
+              onClick={() => {}}
+            >
+              <BsFillPlayFill size={30} />
+            </div>
+          </div>
+          <p className="mt-4 font-semibold text-green-400">New <span className="text-white">2023</span></p>
+          <div className='flex flex-row items-center gap-2 mt-4'>
+            <p className="text-white text-{10px} lg:text-sm">{data.duration}</p>
+          </div>
+          <div className='flex flex-row items-center gap-2 mt-4'>
+            <p className="text-white text-{10px} lg:text-sm">{data.genre}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MovieCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@
 import { NextPageContext } from 'next';
 import {getSession, signOut} from 'next-auth/react';
 import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMoveList';
 
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
@@ -20,9 +23,16 @@ export async function getServerSideProps(context: NextPageContext){
 }
 
 export default function Home() {
+  const {data: movies = []} = useMovieList();
+  
   return (
+    
   <>
    <Navbar />
+   <Billboard />
+   <div className='pb-40'>
+   <MovieList title ="Trending Now" data={movies} />
+   </div>
    </>
   )
 }
